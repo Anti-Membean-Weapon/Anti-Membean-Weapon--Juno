1a808ec2378d99f395e20481a588548b64903218
Anti-Membean Weapon <upload.typhoontechnologies@gmail.com>
2016-01-06 17:44:36 -0500

    Bug Fixes

--- content.js
+++ content.js
@@ -1,22 +1,24 @@
 if(location.href.indexOf("membean.com") > -1){
-		var time = Math.floor((Math.random() * 10100) + 3700);
+		var time = Math.floor((Math.random() * 19100) + 10000);
 		function ans(){
 			if($("form[name='Next']").length){
+				$(".answer").click()
 				$("form[name='Next']").click();
+
 				console.log("Moving on");
-				time = Math.floor((Math.random() * 10100) + 3700);
+				time = Math.floor((Math.random() * 19100) + 10000);
 				return;
 			}else{
 			var correct_chance =  Math.floor(Math.random() * (100 - 0 + 1)) + 0;
-			if(correct_chance < 80){
+			if(correct_chance < 66){
 				document.getElementById('pass').click();
 				console.log("Correct! time was " + time/1000 + "\nNum: " + correct_chance);
-			}else if(correct_chance > 70){
+			}else if(correct_chance > 65){
 				console.log("Random Guess! time was " + time/1000 + "\nNum: " + correct_chance);
 				$(".choice").slice(Math.floor(Math.random() * (3 - 0)) + 0).click();
 			}
 			
-			time = Math.floor((Math.random() * 10100) + 3700);
+			time = Math.floor((Math.random() * 19100) + 10000);
 		}
 		}
 		setInterval(ans, time);
